3dd5ec7c1d291ab1beff38c17f3471e89ba5b49a
Mert Gürhan <mertgurhan27@gmail.com>
2026-02-28 01:46:22 +0300

    Add MATLAB functions and datasets
    Add dis.m (computes quadratic discriminant b^2-4*a*c) and hacim.m (computes sphere volume 4/3*pi*r^3). Add multiple .mat data files (matlab2.mat, matlab3.mat, matlab4.mat, "matlab5 grafik.mat", matlab5.mat, matlab6.mat, matlab7.mat) plus an untitled.m placeholder. These provide utility functions and sample datasets for MATLAB exercises.

---
+++ dis.m
@@ -0,0 +1,8 @@
+function [outputArg1] = dis(inputArg1,inputArg2,inputArg3)
+
+a=inputArg1;
+
+b=inputArg2;
+c=inputArg3;
+outputArg1=b^2-4*a*c;
+end
---
+++ hacim.m
@@ -0,0 +1,6 @@
+function [v] = hacim(inputArg1)
+r=inputArg1;
+v=4/3*pi*r^3;
+
+
+end
