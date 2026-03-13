a9d0444956616e483f66f5c3902befec5a6015f5
Mert Gürhan <mertgurhan27@gmail.com>
2026-03-13 15:07:45 +0300

    13Mart2026

---
+++ matlab12.m
@@ -0,0 +1,65 @@
+Avogadros_constant=6.022e23
+
+Avogadros_constant =
+
+   6.0220e+23
+
+iron_diameter= 140e-12
+
+iron_diameter =
+
+   1.4000e-10
+
+
+   A=5.1
+
+A =
+
+    5.1000
+
+format long 
+A
+
+A =
+
+   5.100000000000000
+
+format bank
+A
+
+A =
+
+          5.10
+
+format short
+A
+
+A =
+
+    5.1000
+
+
+ format short e
+A
+
+A =
+
+   5.1000e+00
+
+format long e
+A
+
+A =
+
+     5.100000000000000e+00
+
+
+
+
+
+% yorum satırı oluyor <3 <d d>
+
+a=5;
+c=a^3+12;
+d=c+a;
+e=sin(30)+d;
