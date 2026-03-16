df60b158e0c035ed1a149257f715e0c54e132ef5
Mert Gürhan <mertgurhan27@gmail.com>
2026-03-17 01:08:32 +0300

    right,left open grafic

---
+++ home/rlopen.m
@@ -0,0 +1,23 @@
+% 1. Evrensel Küme (x ekseni)
+x = 0:0.1:100; 
+
+% 2. Sola Açık Fonksiyon (Örn: "Genç" veya "Soğuk")
+% x=20'ye kadar 1 olsun, x=50'de 0'a insin.
+mu_sol_acik = max(0, min(1, (50 - x) / (50 - 20)));
+
+% 3. Sağa Açık Fonksiyon (Örn: "Yaşlı" veya "Sıcak")
+% x=50'de yükselmeye başlasın, x=80'de 1 olsun.
+mu_sag_acik = max(0, min(1, (x - 50) / (80 - 50)));
+
+figure;
+plot(x,mu_sol_acik,"r","LineWidth",2);hold on;
+plot(y,mu_sag_acik,"b","LineWidth",2);hold on;
+grid on;
+ylim
+
+grid on;
+ylim([-0.1 1.1]); % Üstten ve alttan biraz boşluk
+legend('Sola Açık (Genç)', 'Sağa Açık (Yaşlı)');
+xlabel('Yaş (Evrensel Küme)');
+ylabel('Üyelik Derecesi (\mu)');
+title('Basit Sol Açık ve Sağ Açık Grafik Çizimi');
